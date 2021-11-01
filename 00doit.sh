9dead854fce40c9254376ca13cf0c3ffc228971a
hohno-46466 <hohno.46466@gmail.com>
2021-11-02 06:12:07 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

---
+++ 00doit.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+# Last update: Wed Oct 27 06:38:35 JST 2021 by @hohno_at_kuimc
+
+PNAME=$(basename $0)
+# echo "$PNAME"
+
+TARGET=$HOME/bin
+
+opts="$opts"
+opts="$opts --exclude=${PNAME}"
+opts="$opts --exclude=.Spotlight-V100"
+opts="$opts --exclude=.TemporaryItems"
+opts="$opts --exclude=.fseventsd"
+opts="$opts --exclude=.git*"
+opts="$opts --exclude '*.swp'"
+opts="$opts --exclude '*~'"
+opts="$opts --exclude '*.bak'"
+opts="$opts --exclude '*.md'"
+opts="$opts --exclude '*.txt'"
+
+echo $(echo rsync -avE $@ $opts $(pwd)/ $TARGET)
+eval $(echo rsync -avE $@ $opts $(pwd)/ $TARGET)
